060ebb4bd22a3a213b4df4ee68813e5561cc45bd
VibolSovichea <sovicheavibol9@gmail.com>
2024-07-16 20:29:12 +0700

    variables, type conversion

--- DayOne-Variables/DayOne.js
+++ DayOne-Variables/DayOne.js
@@ -1,3 +1,24 @@
-let stringNumber = "15";
-let newNumber = +stringNumber + 5;
-console.log(newNumber); // 20
+// Variable Declaration
+
+let age = 10;
+let str = "yes";
+let isTrue = false;
+
+// Type Conversion
+/*
+- Implicit Type conversion
+- Explicit Type conversion
+*/
+
+// string to number
+let stofrage = +"234";
+let strofage2 = Number("123");
+
+// number to string
+let number = 10;
+let tostr1 = number+"";
+let tostr2 = number.toString();
+
+// string and number to boolean
+let strtobool = Boolean(str);
+let numtobool = Boolean(number);
