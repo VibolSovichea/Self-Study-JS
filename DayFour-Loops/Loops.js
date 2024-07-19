702360be4877d7f67f915435a78725847593b77b
VibolSovichea <sovicheavibol9@gmail.com>
2024-07-19 19:30:37 +0700

    add loop.js

---
+++ DayFour-Loops/Loops.js
@@ -0,0 +1,26 @@
+// loop statements : for-loop, while-loop.
+// for-loop is use when you know the amount of time needed to loop through.
+
+// for(let i = 0; i <= 10; i++){
+//      console.log("Hello");
+// }
+
+// while loop is use when the amount of time needed to loop is base on specific condition
+
+// let isend = true;
+// while(isCool){
+//      console.log("samnang!");
+// }
+
+// for in statement
+// let array = [12,34,5656,7676];
+// for(let x in array){
+//      console.log("Hello");
+// }
+
+// do-while loop : 
+// let i = 0;
+// do{
+//      console.log("Panha!");
+//      i++;
+// }while(i < 5);
