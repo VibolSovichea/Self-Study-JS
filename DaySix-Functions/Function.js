bd3295d545485d4206c130a7d428080f5f817a41
VibolSovichea <sovicheavibol9@gmail.com>
2024-07-25 19:41:45 +0700

    Add function.js file

---
+++ DaySix-Functions/Function.js
@@ -0,0 +1,9 @@
+// * Exercise Array*
+
+// Create a program that allow teacher to add or remove students.
+// 1. Create a prompt ask user if they want to add or remove.
+// 2. After user pick a choice, prompt user for a name.
+// 3. Print out the array of name to user then ask user if they want to continue or exit.
+// 4. If continue, ask user to pick (add or remove). If exit, end the program.
+
+// Tip : Use pop and push.
